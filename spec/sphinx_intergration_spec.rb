379f240fe89ef93de01c51e20f21a7449cfcfb0a
Masen Matthews and Ron Craig <ron@>
2018-02-13 13:47:57 -0800

    two passing intergration specs, two passing specs

--- spec/sphinx_intergration_spec.rb
+++ spec/sphinx_intergration_spec.rb
@@ -4,11 +4,19 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('Riddle of the Sphinx', {:type => :feature}) do
-  it('processes the user entry and returns whether it is a square or not') do
+  it('processes the user entry and returns correct answers') do
     visit('/')
     fill_in('riddle1', :with => 'a bed')
     fill_in('riddle2', :with => 'the letter v')
     fill_in('riddle3', :with => 'hawaii')
+    click_button('submit')
+    expect(page).to have_content('oh happy days')
+  end
+  it('processes the user entry and returns correct anwers, regardless of capitalization') do
+    visit('/')
+    fill_in('riddle1', :with => 'A bed')
+    fill_in('riddle2', :with => 'THE letter V')
+    fill_in('riddle3', :with => 'HAWaii')
     click_button('submit')
     expect(page).to have_content('oh happy days')
   end
